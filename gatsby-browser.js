118c9f0c0a2dc6c9bf77192a08532e5d73f5e972
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2020-08-04 13:34:24 -1000

    fix password wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,15 +2,18 @@ import React from "react"
 
 import { BackgroundProvider } from "./src/contexts/BackgroundContext"
 import { MenuProvider } from "./src/contexts/MenuContext"
-
 import PasswordWrapper from "./src/components/pw"
 
 import "./src/styles/reset.css"
 
 export const wrapRootElement = ({ element }) => (
   <BackgroundProvider>
-    <MenuProvider>
-      <PasswordWrapper>{element}</PasswordWrapper>
-    </MenuProvider>
+    <MenuProvider>{element}</MenuProvider>
   </BackgroundProvider>
 )
+
+export const wrapPageElement = ({ element, props }) => {
+  // props provide same data to PasswordWrapper as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <PasswordWrapper {...props}>{element}</PasswordWrapper>
+}
