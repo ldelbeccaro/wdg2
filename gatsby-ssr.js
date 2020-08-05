609d3e8d6a379e1f24a8e9c5eae2418f6f5bdb5a
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2020-08-04 14:35:45 -1000

    actually fix password stuff

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+
+import { BackgroundProvider } from "./src/contexts/BackgroundContext"
+import { MenuProvider } from "./src/contexts/MenuContext"
+import PasswordWrapper from "./src/components/pw"
+
+import "./src/styles/reset.css"
+
+export const wrapRootElement = ({ element }) => (
+  <BackgroundProvider>
+    <MenuProvider>{element}</MenuProvider>
+  </BackgroundProvider>
+)
+
+export const wrapPageElement = ({ element, props }) => {
+  // props provide same data to PasswordWrapper as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <PasswordWrapper {...props}>{element}</PasswordWrapper>
+}
