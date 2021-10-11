3f99a1428b6390d9a2138bb90605a40750455e1c
Laura Del Beccaro <ldelbeccaro@users.noreply.github.com>
2021-10-11 11:30:51 -0700

    fix timeline styles

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -27,28 +27,38 @@ const Timeline = () => {
       <div className='column right'>
         <div className='hidden'></div>
         <div className='events'>
-          <div className='event'>
-            <div className='datetime'>
-              <div className='date'>Friday, July 22</div>
-              <div className='time'>7pm</div>
-            </div>
-            <div className='details'>
-              <div className='title'>Welcome party</div>
-              <div className='location'>Terranea Resort • Cielo Point</div>
+          <div className='date'>
+            <div className='date-text'>Friday, July 22</div>
+            <div className='event'>
+              <div className='datetime'>
+                <div className='time'>7pm</div>
+              </div>
+              <div className='details'>
+                <div className='title'>Welcome party</div>
+                <div className='location'>Terranea Resort • Cielo Point</div>
+              </div>
             </div>
           </div>
-          <div className='event'>
-            <div className='datetime'>
-              <div className='date'>Saturday, July 23</div>
-              <div className='time'>4pm - 10pm</div>
-              <div className='time' style={{marginTop:'100px'}}>10pm +</div>
+          <div className='date'>
+            <div className='date-text'>Saturday, July 23</div>
+            <div className='event'>
+              <div className='datetime'>
+                <div className='time'>4pm - 10pm</div>
+              </div>
+              <div className='details'>
+                <div className='title'>Ceremony & Reception</div>
+                <div className='location'>Terranea Resort • Palos Verdes Meadows</div>
+                <div className='notes'>Arrive early for a pre-ceremony cocktail!</div>
+              </div>
             </div>
-            <div className='details'>
-              <div className='title'>Ceremony & Reception</div>
-              <div className='location'>Terranea Resort • Palos Verdes Meadows</div>
-              <div className='notes'>Arrive early for a pre-ceremony cocktail!</div>
-              <div className='title'>After party</div>
-              <div className='location'>TBD</div>
+            <div className='event'>
+              <div className='datetime'>
+                <div className='time'>10pm +</div>
+              </div>
+              <div className='details'>
+                <div className='title'>After party</div>
+                <div className='location'>TBD</div>
+              </div>
             </div>
           </div>
         </div>
